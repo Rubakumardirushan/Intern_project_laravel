22917bce153720926b189e4fe2ef2bddca8e1a67
Rubakumar Dirushan <123752924+Rubakumardirushan@users.noreply.github.com>
2023-09-10 11:05:13 +0530

    Update index.blade.php

--- ABC_COMPANY/resources/views/index.blade.php
+++ ABC_COMPANY/resources/views/index.blade.php
@@ -1,12 +1,1 @@
-<!-- resources/views/products/index.blade.php -->
 
-@extends('layouts.app')
-
-@section('content')
-    <h1>Products</h1>
-    <ul>
-        @foreach($products as $product)
-            <li>{{ $product->name }} - {{ $product->price }}</li>
-        @endforeach
-    </ul>
-@endsection
